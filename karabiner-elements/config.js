097dc28946adae58ee8638389e3ce0ef37ce1713
Kai Grossjohann <kaig@alldimensions.net>
2023-03-12 16:46:31 +0100

    Update for USB keyboard
    This is the Cherry Clean Desktop keyboard.  We need to swap the alt and
    cmd keys, and there is an application key on the right that we change
    into an alt key, as well.

--- karabiner-elements/config.js
+++ karabiner-elements/config.js
@@ -784,7 +784,73 @@ let clnav_rules = {
             }
         ]
     },
-    "devices": [],
+    "devices": [
+        {
+            "disable_built_in_keyboard_if_exists": false,
+            "fn_function_keys": [],
+            "identifiers": {
+                "is_keyboard": true,
+                "is_pointing_device": false,
+                "product_id": 384,
+                "vendor_id": 1130
+            },
+            "ignore": false,
+            "manipulate_caps_lock_led": true,
+            "simple_modifications": [
+                {
+                    "from": {
+                        "key_code": "left_option"
+                    },
+                    "to": [
+                        {
+                            "key_code": "left_command"
+                        }
+                    ]
+                },
+                {
+                    "from": {
+                        "key_code": "left_command"
+                    },
+                    "to": [
+                        {
+                            "key_code": "left_option"
+                        }
+                    ]
+                },
+                {
+                    "from": {
+                        "key_code": "right_option"
+                    },
+                    "to": [
+                        {
+                            "key_code": "right_command"
+                        }
+                    ]
+                },
+                {
+                    "from": {
+                        "key_code": "right_command"
+                    },
+                    "to": [
+                        {
+                            "key_code": "right_option"
+                        }
+                    ]
+                },
+                {
+                    "from": {
+                        "key_code": "application"
+                    },
+                    "to": [
+                        {
+                            "key_code": "right_option"
+                        }
+                    ]
+                }
+            ],
+            "treat_as_built_in_keyboard": false
+        }
+    ],
     "fn_function_keys": [
     ],
     "name": "CL",
